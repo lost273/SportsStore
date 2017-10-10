a299834b5d174e31cb7211d55415acecc56101e0
lost273 <dnk273@gmail.com>
2017-10-10 08:10:51 +0300

    10-10-2017-8-10

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -37,5 +37,11 @@ namespace SportsStore.WebUI.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+        public PartialViewResult Summary(Cart cart) {
+            return PartialView(cart);
+        }
+        public ViewResult Checkout() {
+            return View(new ShippingDetails());
+        }
     }
 }
